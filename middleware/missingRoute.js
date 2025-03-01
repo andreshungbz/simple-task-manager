1dfd67be311babfd58c3049def4aab324640e5a0
Andres Hung <2018118240@ub.edu.bz>
2025-03-01 15:29:56 -0600

    Render a page for errors

--- middleware/missingRoute.js
+++ middleware/missingRoute.js
@@ -2,7 +2,7 @@
 // middleware to handle any non-existing routes; returns a 404 message
 
 const missingRoute = (req, res) => {
-  res.status(404).send('404 Page Not Found');
+  res.render('error', { title: 'Error 404', description: 'Page Not Found' });
 };
 
 export default missingRoute;
