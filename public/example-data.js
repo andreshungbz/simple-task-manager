96c5f030a4f7f6c7850ba5b55daf21153ecccec1
Andres Hung <2018118240@ub.edu.bz>
2025-03-01 12:23:25 -0600

    Add HTML structure to main page

---
+++ public/example-data.js
@@ -0,0 +1,23 @@
+const exampleData = [
+  {
+    id: 1,
+    title: 'Complete OOP Tables',
+    description:
+      'Design database tables for the OOP Final Project and create corresponding ERDs.',
+    completed: false,
+  },
+  {
+    id: 2,
+    title: 'Continue C# Learning',
+    description: 'Read Chapter 4 of the Head First C# texbook.',
+    completed: false,
+  },
+  {
+    id: 3,
+    title: 'Customize Work Profile',
+    description: 'Finish setting up work account profile for internship.',
+    completed: true,
+  },
+];
+
+export default exampleData;
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,11 +2,12 @@
 // routes relating to tasks and processing tasks
 
 import express from 'express';
+import exampleData from '../public/example-data.js';
 
 const taskRoutes = express.Router();
 
 taskRoutes.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { tasks: exampleData });
 });
 
 export default taskRoutes;
