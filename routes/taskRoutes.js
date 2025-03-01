a11efec7472b1dc6e0d118f0a24a91516a985d76
Andres Hung <2018118240@ub.edu.bz>
2025-03-01 16:04:41 -0600

    Implement route for adding tasks

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -5,9 +5,13 @@ import express from 'express';
 import exampleData from '../public/example-data.js';
 
 const taskRoutes = express.Router();
+const tasks = [];
+let id = 1;
+
+// general routes
 
 taskRoutes.get('/', (req, res) => {
-  res.render('index', { tasks: exampleData });
+  res.render('index', { tasks: tasks });
 });
 
 taskRoutes.get('/usage', (req, res) => {
@@ -16,6 +20,30 @@ taskRoutes.get('/usage', (req, res) => {
 
 taskRoutes.get('/about', (req, res) => {
   res.render('about');
+});
+
+// task operations
+
+taskRoutes.post('/add-task', (req, res) => {
+  const { taskTitle, taskDescription } = req.body;
+
+  // handle missing fields
+  if (!taskTitle || !taskDescription) {
+    return res.render('error', {
+      title: 'Error: Missing Field',
+      description:
+        "The content of the task's title and/or description was missing. Task not added.",
+    });
+  }
+
+  tasks.unshift({
+    id: id++,
+    title: taskTitle,
+    description: taskDescription,
+    completed: false,
+  });
+
+  res.redirect('/');
 });
 
 export default taskRoutes;
