51271d8195fbe7a758097bff95cb6ebde1a73e8e
Andres Hung <2018118240@ub.edu.bz>
2025-03-24 12:06:46 -0600

    Refactor construction of task select query string into its own util function

--- src/models/taskModel.ts
+++ src/models/taskModel.ts
@@ -2,50 +2,21 @@
 // database implementation of task operations
 
 import { query } from '../config/database.js';
-import { QueryResult } from 'pg';
+import { createPGTaskSELECT } from '../utils/createPGTaskSELECT.js';
 
+import { QueryResult } from 'pg';
 import { FilterOptions } from '../types/FilterOptions.js';
 import { Priority } from '../types/Priority.js';
 import { Task } from '../types/Task.js';
 
 // READ
 
-export const readTasks = async ({
-  search,
-  category,
-  priorityOrder,
-}: FilterOptions): Promise<QueryResult<any>> => {
-  let queryString = 'SELECT * FROM tasks';
-  const conditions: string[] = [];
-  const values: (string | null)[] = [];
-
-  // apply search filter
-  if (search) {
-    conditions.push('(title ILIKE $1 OR description ILIKE $1)');
-    values.push(`%${search.toLocaleLowerCase()}%`);
-  }
-
-  // apply category filter
-  if (category === 'completed') {
-    conditions.push('completed = true');
-  } else if (category === 'incomplete') {
-    conditions.push('completed = false');
-  }
-
-  // add conditions
-  if (conditions.length > 0) {
-    queryString += ` WHERE ${conditions.join(' AND ')}`;
-  }
-
-  // apply sort
-  if (priorityOrder === 'high') {
-    queryString += ` ORDER BY CASE priority WHEN 'high' THEN 1 WHEN 'medium' THEN 2 WHEN 'low' THEN 3 END ASC`;
-  } else if (priorityOrder === 'low') {
-    queryString += ` ORDER BY CASE priority WHEN 'low' THEN 1 WHEN 'medium' THEN 2 WHEN 'high' THEN 3 END ASC`;
-  }
-
+export const readTasks = async (
+  options: FilterOptions
+): Promise<QueryResult<any>> => {
+  const queryObject = createPGTaskSELECT(options);
   try {
-    const result = await query(queryString, values);
+    const result = await query(queryObject.query, queryObject.values);
     return result;
   } catch (error) {
     console.error('[taskModel/readTasks] Error fetching tasks:', error);
---
+++ src/utils/createPGTaskSELECT.ts
@@ -0,0 +1,43 @@
+// Filename: createQueryString.ts
+// function that takes search, category, and priority order parameters
+// and returns and object with the SQL query string and values for PG
+
+import { FilterOptions } from '../types/FilterOptions.js';
+
+export const createPGTaskSELECT = ({
+  search,
+  category,
+  priorityOrder,
+}: FilterOptions): { query: string; values: (string | null)[] } => {
+  // initial variables
+  let query = 'SELECT * FROM tasks';
+  const conditions: string[] = [];
+  const values: (string | null)[] = [];
+
+  // apply search filter
+  if (search) {
+    conditions.push('(title ILIKE $1 OR description ILIKE $1)');
+    values.push(`%${search.toLocaleLowerCase()}%`);
+  }
+
+  // apply category filter
+  if (category === 'completed') {
+    conditions.push('completed = true');
+  } else if (category === 'incomplete') {
+    conditions.push('completed = false');
+  }
+
+  // add WHERE conditions
+  if (conditions.length > 0) {
+    query += ` WHERE ${conditions.join(' AND ')}`;
+  }
+
+  // apply ORDER sort
+  if (priorityOrder === 'high') {
+    query += ` ORDER BY CASE priority WHEN 'high' THEN 1 WHEN 'medium' THEN 2 WHEN 'low' THEN 3 END ASC`;
+  } else if (priorityOrder === 'low') {
+    query += ` ORDER BY CASE priority WHEN 'low' THEN 1 WHEN 'medium' THEN 2 WHEN 'high' THEN 3 END ASC`;
+  }
+
+  return { query, values };
+};
